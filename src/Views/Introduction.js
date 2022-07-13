9cef0b76c28770714d390d7e87dc6a2e0ff172cd
pinkishincoloragain <smb1103@gmail.com>
2022-07-14 00:06:58 +0900

    changed Intro

--- src/Views/Introduction.js
+++ src/Views/Introduction.js
@@ -24,23 +24,13 @@ const Card = ({ props }) => {
   return (
     <CardWrapper className="max-w-md rounded overflow-hidden shadow-xl align-middle justify-center bg-dark-background my-8 ">
       <div className="flex align-middle justify-center">
-        <img className="w-4/12" src={reactGif} alt="Sunset in the mountains" />
+        <img className="w-4/12" src={props.src} alt={props.title + " image"} />
       </div>
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{props.title}</div>
         {Expls}
       </div>
-      <div className="px-6 pt-4 pb-2">
-        {/* <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-          #photography
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-          #travel
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-          #winter
-        </span> */}
-      </div>
+      <div className="px-6 pt-4 pb-2"></div>
     </CardWrapper>
   );
 };
@@ -71,11 +61,11 @@ export default function Introduction(props) {
 
   const BeatifulBar = styled(`div`)({
     backgroundImage:
-      "linear-gradient(217deg, rgba(255,0,0,.8), rgba(255,0,0,0) 70.71%),linear-gradient(127deg, rgba(0,255,0,.8), rgba(0,255,0,0) 70.71%),linear-gradient(336deg, rgba(0,0,255,.8), rgba(0,0,255,0) 70.71%)",
+      "linear-gradient(217deg, #231955, #1F4690 70.71%),linear-gradient(127deg, rgba(0,255,0,.8), rgba(0,255,0,0) 70.71%),linear-gradient(336deg, rgba(0,0,255,.8), rgba(0,0,255,0) 70.71%)",
   });
   return (
     <div className="w-full flex flex-wrap justify-between">
-      <BeatifulBar className="w-full h-4 "></BeatifulBar>
+      <BeatifulBar className="w-full h-2 "></BeatifulBar>
       <div className="w-full h-4 bg-gradient-to-r from-indigo-500"></div>
       <Card props={education} />
       <Card props={experience} />
