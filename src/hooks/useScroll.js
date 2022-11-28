76ef7931376af6fc6fd347135ac9fe89872e1e11
pinkishincoloragain <smb1103@gmail.com>
2022-11-28 16:03:08 +0900

    #3 feat: applied eslint to hooks

--- src/hooks/useScroll.js
+++ src/hooks/useScroll.js
@@ -2,53 +2,53 @@ import { useRef, useEffect, useCallback } from "react";
 import { useSpring } from "react-spring";
 
 const useScroll = (type = "up", threshold, delay = 0) => {
-	const element = useRef();
+  const element = useRef();
 
-	const handleDirection = name => {
-		switch (name) {
-			case "up":
-				return "translate3d(0, 50%, 0)";
-			case "down":
-				return "translate3d(0, -50%, 0)";
-			case "left":
-				return "translate3d(50%, 0, 0)";
-			case "right":
-				return "translate3d(-50%, 0, 0)";
-			default:
-				return;
-		}
-	};
+  const handleDirection = name => {
+    switch (name) {
+      case "up":
+        return "translate3d(0, 50%, 0)";
+      case "down":
+        return "translate3d(0, -50%, 0)";
+      case "left":
+        return "translate3d(50%, 0, 0)";
+      case "right":
+        return "translate3d(-50%, 0, 0)";
+      default:
+        return;
+    }
+  };
 
-	const onScroll = useCallback(
-		([entry]) => {
-			const { current } = element;
-			if (entry.isIntersecting) {
-				current.style.transitionProperty = "all";
-				current.style.transitionDuration = `0.3s`;
-				current.style.transitionTimingFunction = "cubic-bezier(0, 0, 0.2, 1)";
-				current.style.transitionDelay = `${delay}s`;
-				current.style.opacity = 1;
-				current.style.transform = "translate3d(0, 0, 0)";
-			}
-		},
-		[delay]
-	);
+  const onScroll = useCallback(
+    ([entry]) => {
+      const { current } = element;
+      if (entry.isIntersecting) {
+        current.style.transitionProperty = "all";
+        current.style.transitionDuration = `0.3s`;
+        current.style.transitionTimingFunction = "cubic-bezier(0, 0, 0.2, 1)";
+        current.style.transitionDelay = `${delay}s`;
+        current.style.opacity = 1;
+        current.style.transform = "translate3d(0, 0, 0)";
+      }
+    },
+    [delay]
+  );
 
-	useEffect(() => {
-		let observer;
+  useEffect(() => {
+    let observer;
 
-		if (element.current) {
-			observer = new IntersectionObserver(onScroll, { threshold: threshold });
-			observer.observe(element.current);
-		}
+    if (element.current) {
+      observer = new IntersectionObserver(onScroll, { threshold: threshold });
+      observer.observe(element.current);
+    }
 
-		return () => observer && observer.disconnect();
-	}, [onScroll]);
+    return () => observer && observer.disconnect();
+  }, [onScroll]);
 
-	return {
-		ref: element,
-		style: { opacity: 0, transform: handleDirection(type) },
-	};
+  return {
+    ref: element,
+    style: { opacity: 0, transform: handleDirection(type) },
+  };
 };
 
 export default useScroll;
--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -1,29 +1,29 @@
 import * as React from "react";
 
 const useWindowSize = () => {
-	const [windowSize, setWindowSize] = React.useState({
-		width: window.innerWidth,
-		height: window.innerWidth,
-	});
-	React.useEffect(() => {
-		let resizeTimer;
-		let windowSizer = () => {
-			clearTimeout(resizeTimer);
-			resizeTimer = setTimeout(() => {
-				setWindowSize({
-					width: document.body.clientWidth,
-					height: document.body.clientHeight,
-				});
-			}, 100);
-		};
-		window.addEventListener("resize", windowSizer);
+  const [windowSize, setWindowSize] = React.useState({
+    width: window.innerWidth,
+    height: window.innerWidth,
+  });
+  React.useEffect(() => {
+    let resizeTimer;
+    let windowSizer = () => {
+      clearTimeout(resizeTimer);
+      resizeTimer = setTimeout(() => {
+        setWindowSize({
+          width: document.body.clientWidth,
+          height: document.body.clientHeight,
+        });
+      }, 100);
+    };
+    window.addEventListener("resize", windowSizer);
 
-		return () => {
-			window.removeEventListener("resize", windowSizer);
-		};
-	}, [windowSize]);
+    return () => {
+      window.removeEventListener("resize", windowSizer);
+    };
+  }, [windowSize]);
 
-	return windowSize;
+  return windowSize;
 };
 
 export default useWindowSize;
