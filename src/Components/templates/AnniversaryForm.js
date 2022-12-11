ea0891ee34407d19cb9df0902ace641102307f16
pinkishincoloragain <smb1103@gmail.com>
2022-12-12 02:28:22 +0900

    #7 feat: added delegation(extract) func

--- src/Components/templates/AnniversaryForm.js
+++ src/Components/templates/AnniversaryForm.js
@@ -19,27 +19,46 @@ const MandatoryField = styled.span({
 });
 
 export default function AnniversaryForm() {
+  const [response, setResponse] = React.useState({});
+
+  const extractFormData = e => {
+    const formData = {
+      reaction: [],
+    };
+
+    return [...e.target]
+      .filter(el => el.id !== "" || el.value !== "false")
+      .reduce((acc, el) => {
+        if (el.id === "") return { ...acc, reaction: [...acc.reaction, el.value] };
+        else return { ...acc, [el.id]: el.value };
+      }, formData);
+  };
+
   const handleSubmit = e => {
     e.preventDefault();
-    console.log("form submitted");
+    setResponse(extractFormData(e));
   };
-  
+
   return (
     <AnniversaryFormWrapper onSubmit={e => handleSubmit(e)}>
       <H2>{anniversary.title}</H2>
       <br />
       <H3>{anniversary.questions[0]}</H3>
-      <MultiSelectForm options={anniversary.options} addFormPlaceholder={anniversary.createByOwn} />
+      <MultiSelectForm
+        id={"tryAdd"}
+        options={anniversary.options}
+        addFormPlaceholder={anniversary.createByOwn}
+      />
       <H3>{anniversary.questions[1]}</H3>
-      <InputForm placeholder={anniversary.placeholder.name} />
+      <InputForm id={"name"} placeholder={anniversary.placeholder.name} />
       <H3>
         {anniversary.questions[2]}
         <MandatoryField>*</MandatoryField>
       </H3>
-      <InputForm placeholder={anniversary.placeholder.email} />
+      <InputForm id={"tel"} placeholder={anniversary.placeholder.email} />
       <H3>{anniversary.questions[3]}</H3>
-      <TextAreaForm placeholder={anniversary.placeholder.message} />
-      <SubmitButton>{anniversary.submit}</SubmitButton>
+      <TextAreaForm id={"description"} placeholder={anniversary.placeholder.message} />
+      <SubmitButton value={false}>{anniversary.submit}</SubmitButton>
     </AnniversaryFormWrapper>
   );
 }
