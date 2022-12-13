f7934674ea011c5d6bf5d0d21ee1bc075846e4ce
pinkishincoloragain <smb1103@gmail.com>
2022-12-13 20:26:10 +0900

    #11 feat: added ThankyouPage

---
+++ src/Components/templates/ThankyouContent.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+import { H2 } from "../atoms/Text";
+
+export default function ThankyouContent() {
+  return (
+    <>
+      <H2>Thank you for your response!</H2>
+    </>
+  );
+}
---
+++ src/Views/Thankyou.js
@@ -0,0 +1,25 @@
+import * as React from "react";
+import styled from "styled-components";
+import colors from "../Components/colors.json";
+
+import ThankyouContent from "../Components/templates/ThankyouContent";
+
+const ThankyouWrapper = styled.div(props => {
+  return {
+    padding: "10vh 0 10vh 0",
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: colors.dark.background,
+    color: colors.white,
+  };
+});
+
+export default function Thankyou() {
+  return (
+    <ThankyouWrapper>
+      <ThankyouContent />
+    </ThankyouWrapper>
+  );
+}
