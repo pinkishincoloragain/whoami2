c82f6c1419b858799d4d5c411c4febd86b09a282
Chaejun Lee <chaejunlee@knu.ac.kr>
2022-12-18 16:41:47 +0900

    #15 feat: added reverse props to BeautifulBar

--- src/Components/atoms/BeautifulBar.js
+++ src/Components/atoms/BeautifulBar.js
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 import color from "../colors.json";
 
-const BeautifulBar = styled(`div`)({
-  backgroundImage: `linear-gradient(90deg, ${color.dark.gold}, transparent)`,
-  width: "100%",
+const BeautifulBar = styled(`div`)(props => ({
+  backgroundImage: `linear-gradient(${props.reverse ? 270 : 90}deg, ${color.dark.gold}, transparent)`,
+  width: "50%",
   height: "0.125rem",
   borderRadius: "0.125rem",
   marginTop: "1vh",
   marginBottom: "1vh",
-});
+}));
 
 export default BeautifulBar;
