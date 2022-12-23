efd08a5af69879e0dc4f3965018923ad72b0c630
pinkishincoloragain <smb1103@gmail.com>
2022-12-23 16:13:39 +0900

    #23 feat: added NavigateContent to let user navigate to Sharing page

---
+++ src/Components/organisms/NavigateContent.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+
+import { LinkButton, SelectButton } from "../atoms/MyButton";
+import ShareLetterBox from "../molecules/ShareLetterBox";
+
+const NavigateContentWrapper = styled.div({
+  marginTop: "20vh",
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-between",
+});
+
+export default function NavigateContent() {
+  const [isSharingMode, setIsSharingMode] = React.useState(false);
+
+  const handleShareButtonClick = () => {
+    setIsSharingMode(true);
+  };
+
+  return (
+    <NavigateContentWrapper>
+      <LinkButton to='/'>홈으로 가기</LinkButton>
+      <LinkButton to='/share'>내 편지함 공유하기</LinkButton>
+    </NavigateContentWrapper>
+  );
+}
