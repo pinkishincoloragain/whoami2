71e1f879cf2f9fbe9d2a1426d4fc3990cb04306a
Chaejun Lee <chaejunlee@knu.ac.kr>
2022-12-23 23:54:52 +0900

    #19 feat: moved Waves to the back

--- src/Components/molecules/AnniversaryPopupContent.js
+++ src/Components/molecules/AnniversaryPopupContent.js
@@ -8,22 +8,26 @@ import { Emphasize, H1, H2, H3, H4 } from "../atoms/Text";
 import Waves from "./objects/Waves";
 
 const WavesWrapper = styled.div({
-  marginBlock: "1rem",
-  marginInline: "auto",
-  paddingInline: "0.5rem",
-  width: "fit-content",
+  position: "absolute",
+  display: "flex",
+  alignItems: "center",
+
+  inset: "0",
+  height: "100%",
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
-  aspectRatio: "1 / 1",
-  zIndex: "-1",
-  backgroundColor: "white",
   borderRadius: "50%",
+
+  zIndex: "-1",
 });
 
 export default function AnniversaryPopupContent() {
   return (
     <div>
+      <WavesWrapper>
+        <Waves width={600} height={600} elevation='20' azimuth='45' />
+      </WavesWrapper>
       <H2>
         <Emphasize>{anniversary.mainTitle1}</Emphasize>
         <br />
@@ -31,9 +35,6 @@ export default function AnniversaryPopupContent() {
         <br />
         <div >{anniversary.mainTitle3} <BeautifulBar reverse /> </div>
       </H2>
-      <WavesWrapper>
-        <Waves width={270} height={270} elevation='20' azimuth='45' />
-      </WavesWrapper>
       <H3>{anniversary.subtitle}</H3>
       <H4>{anniversary.description1}</H4>
       <H4>{anniversary.description2}</H4>
--- src/Components/organisms/PopupContent.js
+++ src/Components/organisms/PopupContent.js
@@ -4,6 +4,7 @@ import AnniversaryPopupContent from "../molecules/AnniversaryPopupContent";
 import styled from "styled-components";
 
 const ContentWrapper = styled.div({
+  position: "relative",
   display: "flex",
   flexDirection: "column",
   paddingInline: "1.5rem",
@@ -12,6 +13,8 @@ const ContentWrapper = styled.div({
   width: "100%",
   // height: "100%",
   color: "white",
+  overflow: "hidden",
+  height: "100%",
 });
 
 export default function PopupContent({ children }) {
