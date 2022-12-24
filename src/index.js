19d006ca20da0201ff5fc634ee8aefe388510ec4
pinkishincoloragain <smb1103@gmail.com>
2022-12-24 20:43:14 +0900

    #31 feat: applied PWA assets / features

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { RecoilRoot } from "recoil";
+import * as serviceWorker from "./serviceWorker";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -12,6 +13,7 @@ root.render(
     </RecoilRoot>
   </React.StrictMode>
 );
+serviceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
