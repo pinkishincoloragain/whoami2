750dad1b8f2e90b59735121f12059523946171d5
pinkishincoloragain <smb1103@gmail.com>
2022-12-28 01:06:42 +0900

    #34 feat: modified hooks to use Recoil value

--- src/hooks/useSharableLink.js
+++ src/hooks/useSharableLink.js
@@ -1,8 +1,8 @@
 import { useRecoilValue } from "recoil";
-import { userInfoState } from "../Components/utils/recoil/authRecoil";
+import { userIdState } from "../Components/utils/recoil/authRecoil";
 
 const useSharableLink = () => {
-  const uid = useRecoilValue(userInfoState).uid;
+  const uid = useRecoilValue(userIdState);
   const link = document.location.origin + "/anniversary/" + uid;
   const copyToClipboard = () => {
     navigator.clipboard.writeText(link);
