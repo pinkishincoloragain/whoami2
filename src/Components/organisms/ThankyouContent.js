7bf10227b897ebf7b38283fc3e4378197d2cebc3
pinkishincoloragain <smb1103@gmail.com>
2022-12-27 19:35:58 +0900

    #34 feat: added Navigate home after addResponse

--- src/Components/organisms/ThankyouContent.js
+++ src/Components/organisms/ThankyouContent.js
@@ -1,10 +1,25 @@
 import React from "react";
 import { H2 } from "../atoms/Text";
+import { LargeButton } from "../atoms/MyButton";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
+const ThankyouWrapper = styled.div({
+  width: "80%",
+  height: "40%",
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  flexDirection: "column",
+});
 
 export default function ThankyouContent() {
+  const navigate = useNavigate();
+
   return (
-    <>
+    <ThankyouWrapper>
       <H2>Thank you for your response!</H2>
-    </>
+      <LargeButton onClick={() => navigate("/")}>Home</LargeButton>
+    </ThankyouWrapper>
   );
 }
